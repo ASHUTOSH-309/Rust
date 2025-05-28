221f2a46149572508f9f0a7e34f5d685916b681f
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2025-05-29 00:22:45 +0530

    Buggy as str is locked for read and write operations after being borrowed mutablly

---
+++ src/mutable.rs
@@ -0,0 +1,18 @@
+
+
+fn references(){
+    let mut str: String= String :: from("Ashutosh");
+    let mutable_reference = &mut str;
+
+    // mutable borrow locks the original variable 
+
+    println!("Before Manipulating through reference {}", str);
+    mutable_reference.push_str("Sharma"); // Inserting to the original varibale using a mutable reference 
+    println!("After Manipulating by reference {}", str);
+}
+
+
+
+
+
+
